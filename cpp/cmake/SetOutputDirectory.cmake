37018a37cc615a60932dbe8619d34646aa0a716e
Sergey Pavlov <sergey.pavlov@quantori.com>
2022-05-21 13:07:36 +0400

    fixed output directory

--- cpp/cmake/SetOutputDirectory.cmake
+++ cpp/cmake/SetOutputDirectory.cmake
@@ -1,27 +1,6 @@
 function(set_output_directory target)
-    
-    # COPY AND PASTE FROM INDIGO setup.cmake
-    ###############################################################################
-    # If cross-compiling, we are able to set another OS
-    if (NOT CMAKE_SYSTEM_NAME_LOWER)
-        string(TOLOWER ${CMAKE_SYSTEM_NAME} CMAKE_SYSTEM_NAME_LOWER)
-    endif()
-    # If cross-compiling, we are able to set another Arch, like "aarch64"
-    if (NOT CMAKE_SYSTEM_PROCESSOR_LOWER)
-        string(TOLOWER ${CMAKE_SYSTEM_PROCESSOR} CMAKE_SYSTEM_PROCESSOR_LOWER)
-    endif()
-    if (CMAKE_SYSTEM_PROCESSOR_LOWER STREQUAL "amd64")
-        set(CMAKE_SYSTEM_PROCESSOR_LOWER "x86_64")
-    endif()
-    if (CMAKE_SYSTEM_PROCESSOR_LOWER STREQUAL "arm64")
-        set(CMAKE_SYSTEM_PROCESSOR_LOWER "aarch64")
-    endif()
-    if (CMAKE_SYSTEM_NAME_LOWER STREQUAL "msys")
-        set(CMAKE_SYSTEM_NAME_LOWER "windows")
-    endif()
-    ###############################################################################
 
-    set(${target}_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/../dist/lib/${CMAKE_SYSTEM_NAME_LOWER}-${CMAKE_SYSTEM_PROCESSOR_LOWER})
+    set(${target}_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/../build/bin/)
 
     set_target_properties(${target} PROPERTIES
                       RUNTIME_OUTPUT_DIRECTORY_DEBUG ${${target}_RUNTIME_OUTPUT_DIRECTORY}
