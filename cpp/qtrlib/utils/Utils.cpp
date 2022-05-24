e9f4daf1429dcd892f784fd8e9f13a85ca8b6dd2
ivan.vasilyev <ivanvasilyevwork@gmail.com>
2022-05-24 13:52:33 +0300

    Add parsing arguments

--- cpp/qtrlib/utils/Utils.cpp
+++ cpp/qtrlib/utils/Utils.cpp
@@ -5,3 +5,10 @@ bool endsWith(const std::string &a, const std::string &b) {
     return a.size() >= b.size() &&
            (0 == a.compare(a.size() - b.size(), b.size(), b));
 }
+
+void emptyArgument(const std::string &argument,const std::string &message) {
+    if (argument.empty()) {
+        std::cout << message;
+        exit(1);
+    }
+}
--- cpp/qtrlib/utils/Utils.h
+++ cpp/qtrlib/utils/Utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 
 /**
  * Check, if string a ends with string b.
@@ -9,3 +10,12 @@
  * @return true, if b is end of string a, false otherwise.
  */
 bool endsWith(const std::string &a, const std::string &b);
+
+
+/**
+ * Print message if argument is empty and finish program
+ * @param argument
+ * @param message
+ */
+void emptyArgument(const std::string &argument, const std::string &message);
+
