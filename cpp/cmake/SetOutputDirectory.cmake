210015efa4a6e2c213276a7f066d9ae7177810e7
Sergey Pavlov <sergey.pavlov@quantori.com>
2022-05-26 14:27:00 +0300

    use CMAKE_CURRENT_BINARY_DIR variable

--- cpp/cmake/SetOutputDirectory.cmake
+++ cpp/cmake/SetOutputDirectory.cmake
@@ -1,11 +1,5 @@
 function(set_output_directory target)
 
-    set(${target}_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/../build/bin/)
-
-    set_target_properties(${target} PROPERTIES
-                      RUNTIME_OUTPUT_DIRECTORY_DEBUG ${${target}_RUNTIME_OUTPUT_DIRECTORY}
-                      RUNTIME_OUTPUT_DIRECTORY_RELEASE ${${target}_RUNTIME_OUTPUT_DIRECTORY}
-                      RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL ${${target}_RUNTIME_OUTPUT_DIRECTORY}
-                      RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO ${${target}_RUNTIME_OUTPUT_DIRECTORY})
+    set_target_properties(${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/../bin/)
 
 endfunction()
