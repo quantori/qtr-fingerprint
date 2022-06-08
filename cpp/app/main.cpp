7924e12666fd1790b766c99bbd5d09a97aedecb9
Sergey Pavlov <sergey.pavlov@quantori.com>
2022-06-08 16:13:13 +0300

    throw invalid_argument exception in SearchEngineFactory

--- cpp/app/main.cpp
+++ cpp/app/main.cpp
@@ -20,26 +20,33 @@ ABSL_FLAG(std::string, search_engine_type, "bingo",
           "Search engine type");
 
 int main(int argc, char *argv[]) {
+    
     google::InitGoogleLogging(argv[0]);
     absl::ParseCommandLine(argc, argv);
+
     std::string pathToQuery = absl::GetFlag(FLAGS_path_to_query);
     std::string databasePath = absl::GetFlag(FLAGS_database_path);
     std::string searchEngineType = absl::GetFlag(FLAGS_search_engine_type);
+
     emptyArgument(pathToQuery, "Please specify path_to_query option");
     emptyArgument(databasePath, "Please specify database_path option");
     emptyArgument(searchEngineType, "Please specify search_engine_type option");
+
     IndigoSessionPtr indigoSessionPtr = IndigoSession::create();
     int queryMoleculeId = indigoLoadQueryMoleculeFromFile(pathToQuery.c_str());
     auto queryMolecule = IndigoQueryMolecule(queryMoleculeId, indigoSessionPtr);
+
     SearchEngineFactory::SearchEngineType seType = SearchEngineFactory::EXHAUSTIVE;
     if (searchEngineType == "bingo")
         seType = SearchEngineFactory::BINGO;
     std::shared_ptr<SearchEngineInterface> searchEngine = SearchEngineFactory::create(seType, indigoSessionPtr);
+
     std::vector<IndigoQueryMolecule> queries;
     for (size_t i = 0; i < 10; ++i) {
         queries.emplace_back(queryMolecule);
     }
-    SearchEngineProfiler p(databasePath, *searchEngine);
+
     CompleteSearchEngineProfiler::profile(databasePath, *searchEngine, queries);
+    
     return 0;
 }
