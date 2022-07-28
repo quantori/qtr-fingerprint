fc2a6476c05d56a4868bc9cabb01d2fe77a64880
Seva-Vaskin <vaskin.2003@gmail.com>
2022-07-28 16:17:23 +0300

    fix SpeedTestStat bug

--- notebooks/fp_utils/tests/speed_test_stat.py
+++ notebooks/fp_utils/tests/speed_test_stat.py
@@ -13,29 +13,29 @@ class SpeedTestStat:
     def measured_parameters(self) -> List[str]:
         return ['min', 'max', 'mean', 'median']
 
-    def __init__(self, measurements: Optional[Dict[Type[Finder], List[CatchTime]]] = None):
-        self.measurements: Dict[Type[Finder], List[CatchTime]] = measurements or dict()
+    def __init__(self, measurements: Optional[Dict[str, List[CatchTime]]] = None):
+        self.measurements: Dict[str, List[CatchTime]] = measurements or dict()
 
-    def __get_times(self, finder_class: Type[Finder]) -> List[float]:
-        return list(map(lambda x: x.time, self.measurements[finder_class]))
+    def __get_times(self, finder_name: str) -> List[float]:
+        return list(map(lambda x: x.time, self.measurements[finder_name]))
 
-    def __collect_stat(self) -> Dict[Type[Finder], Dict[str, CatchTime]]:
+    def __collect_stat(self) -> Dict[str, Dict[str, CatchTime]]:
         stat = dict()
-        for finder_class in self.measurements:
-            times = np.array(self.__get_times(finder_class))
-            stat[finder_class] = dict()
+        for finder_name in self.measurements:
+            times = np.array(self.__get_times(finder_name))
+            stat[finder_name] = dict()
             for parameter in self.measured_parameters:
                 value = getattr(np, parameter)(times)
-                stat[finder_class][parameter] = value
+                stat[finder_name][parameter] = value
         return stat
 
     def __str__(self):
         stat = self.__collect_stat()
         res = []
-        for engine_class in stat:
-            res += [f'{engine_class.__name__}:']
+        for finder_name in stat:
+            res += [f'{finder_name}:']
             for parameter in self.measured_parameters:
-                res += [f'\t{stat[engine_class][parameter]:.3f} -- {parameter}']
+                res += [f'\t{stat[finder_name][parameter]:.3f} -- {parameter}']
         return '\n'.join(res)
 
     def __repr__(self):
@@ -69,20 +69,14 @@ class SpeedTestStat:
         ax.boxplot(data, vert=True, patch_artist=True, labels=labels)
         plt.show()
 
-    def drop(self, finders: List[Union[Finder, Type[Finder]]]) -> SpeedTestStat:
+    def drop(self, finders: List[Finder]) -> SpeedTestStat:
         new_stat = deepcopy(self)
         for finder in finders:
-            if isinstance(finder, Finder):
-                finder = finder.__class__
-            assert issubclass(finder, Finder)
-            new_stat.measurements.pop(finder, None)
+            new_stat.measurements.pop(finder.__name__, None)
         return new_stat
 
-    def take(self, finders: List[Union[Finder, Type[Finder]]]) -> SpeedTestStat:
+    def take(self, finders: List[Finder]) -> SpeedTestStat:
         new_stat = SpeedTestStat()
         for finder in finders:
-            if isinstance(finder, Finder):
-                finder = finder.__class__
-            assert issubclass(finder, Finder)
-            new_stat.measurements[finder] = self.measurements[finder]
+            new_stat.measurements[finder.__name__] = self.measurements[finder.__name__]
         return new_stat
