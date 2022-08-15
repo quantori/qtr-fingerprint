b3d69edabb712a923da12e9437c75d0d7fdfe788
Seva-Vaskin <vaskin.2003@gmail.com>
2022-08-15 15:51:22 +0300

    add async search

--- notebooks/substrucure_finder/search_engine.py
+++ notebooks/substrucure_finder/search_engine.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from typing import Generator
+from typing import Generator, Iterable
+import asyncio
 
 from substrucure_finder import utils
 from substrucure_finder.fingerprint import Fingerprint
@@ -18,3 +19,16 @@ class SearchEngine:
         for bucket in self.splitter_tree.get_buckets(fingerprint):
             bucket_search_engine = BucketSearchEngine.load(utils.bucket_path(self.data_path, bucket))
             yield from bucket_search_engine.search(fingerprint, self.data_path, bucket)
+
+    async def __search_in_bucket(self, fingerprint: Fingerprint, bucket: int) -> Iterable[str]:
+        bucket_search_engine = BucketSearchEngine.load(utils.bucket_path(self.data_path, bucket))
+        return bucket_search_engine.search(fingerprint, self.data_path, bucket)
+
+    def async_search(self, fingerprint: Fingerprint) -> Generator[str, None, None]:
+        assert len(fingerprint) == consts.fingerprint_size_in_bits
+
+        buckets = self.splitter_tree.get_buckets(fingerprint)
+        tasks = [asyncio.create_task(self.__search_in_bucket(fingerprint, bucket) for bucket in buckets)]
+
+        for task in tasks:
+            yield from await task
