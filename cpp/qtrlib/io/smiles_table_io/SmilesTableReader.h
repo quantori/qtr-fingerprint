4bc6ff49ca46f393c824e053563dcccd8b89ecee
Seva-Vaskin <vaskin.2003@gmail.com>
2022-09-01 14:19:15 +0300

    add smiles table reader and writer

---
+++ cpp/qtrlib/io/smiles_table_io/SmilesTableReader.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <string>
+
+#include "basic_io/BasicReader.h"
+
+namespace qtr {
+
+    // TODO: test this class
+    class SmilesTableReader : public BasicReader<std::pair<uint64_t, std::string>, SmilesTableReader> {
+    private:
+        uint64_t _smilesInStream;
+
+    public:
+        explicit SmilesTableReader(std::istream *stream) : BaseReader(stream), _smilesInStream(0) {
+            _stream->read((char *) &_smilesInStream, sizeof _smilesInStream);
+        }
+
+        explicit SmilesTableReader(const std::filesystem::path &fileName)
+                : SmilesTableReader(new std::ifstream(fileName)) {
+            LOG(INFO) << "Create SMILES table reader with " << _smilesInStream << " SMILES (" << _stream << ")";
+        }
+
+        ~SmilesTableReader() override {
+            LOG(INFO) << "Delete SMILES table reader (" << _stream << ")";
+        }
+
+        ReadValue readOne() override {
+            uint64_t id;
+            _stream->read((char *) &id, sizeof id);
+            std::string smiles;
+            char symbol;
+            while ((symbol = (char) _stream->get()) != '\n') {
+                smiles += symbol;
+            }
+            return {id, smiles};
+        }
+
+        bool isEof() const override {
+            return _smilesInStream == 0;
+        }
+    };
+
+
+} // qtr
---
+++ cpp/qtrlib/io/smiles_table_io/SmilesTableWriter.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+
+#include "glog/logging.h"
+
+#include "basic_io/BasicWriter.h"
+
+namespace qtr {
+
+    // TODO test this class
+    class SmilesTableWriter : public BasicWriter<std::pair<uint64_t, std::string>, SmilesTableWriter> {
+    private:
+        uint64_t _writtenSmiles;
+
+    public:
+        explicit SmilesTableWriter(std::ostream *stream) : BaseWriter(stream), _writtenSmiles(0) {
+            _stream->write((char *) &_writtenSmiles, sizeof _writtenSmiles); // reserve space for table size
+        }
+
+        explicit SmilesTableWriter(const std::filesystem::path &fileName)
+                : SmilesTableWriter(new std::ofstream(fileName)) {
+            LOG(INFO) << "Create SMILES talbe writer to " << fileName << " (" << _stream << ")";
+        }
+
+        ~SmilesTableWriter() override {
+            _stream->seekp(0, std::ios::beg);
+            _stream->write((char *) &_writtenSmiles, sizeof _writtenSmiles); // write bucket size
+            LOG(INFO) << "Delete SMILES table writer with " << _writtenSmiles << " molecules (" << _stream << ")";
+        }
+
+        void write(const WriteValue &value) override {
+            _writtenSmiles++;
+            auto &[id, smiles] = value;
+            _stream->write((char *) &id, sizeof id);
+            *_stream << smiles << '\n';
+        }
+    };
+
+} // qtr
