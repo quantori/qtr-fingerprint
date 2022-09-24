60660389149b17513572efae7c8830e1acb986e7
Seva-Vaskin <vaskin.2003@gmail.com>
2022-09-24 22:36:49 +0300

    add system for collecting statistics

--- cpp/db_console_queries/main.cpp
+++ cpp/db_console_queries/main.cpp
@@ -1,5 +1,7 @@
 #include <filesystem>
 #include <string>
+#include <algorithm>
+#include <numeric>
 
 #include <glog/logging.h>
 #include <absl/flags/flag.h>
@@ -23,6 +25,15 @@ ABSL_FLAG(std::string, db_name, "",
 ABSL_FLAG(uint64_t, start_search_depth, -1,
           "Depth for start search from. There will be 2^start_search_depth threads.");
 
+ABSL_FLAG(std::string, mode, "",
+          "possible modes: interactive, from_file");
+
+ABSL_FLAG(std::string, input_file, "",
+          "file to load test molecules from");
+
+ABSL_FLAG(std::uint64_t, ans_count, -1,
+          "how many answers program will find");
+
 void initLogging(int argc, char *argv[]) {
     google::InitGoogleLogging(argv[0]);
     google::SetLogDestination(google::INFO, "db_console_queries.info");
@@ -30,10 +41,19 @@ void initLogging(int argc, char *argv[]) {
 }
 
 struct Args {
+
+    enum class Mode {
+        Interactive,
+        FromFile
+    };
+
     std::vector<std::filesystem::path> dataDirPaths;
     std::filesystem::path otherDataPath;
     std::string dbName;
     uint64_t startSearchDepth;
+    Mode mode;
+    std::filesystem::path inputFile;
+    uint64_t ansCount;
 
     std::vector<std::filesystem::path> dbDataDirsPaths;
     std::filesystem::path dbOtherDataPath;
@@ -67,6 +87,21 @@ struct Args {
         }
         LOG(INFO) << "startSearchDepth: " << startSearchDepth;
 
+        std::string modeStr = absl::GetFlag(FLAGS_mode);
+        qtr::emptyArgument(modeStr, "Please specify mode option");
+        inputFile = absl::GetFlag(FLAGS_input_file);
+        if (modeStr == "interactive") {
+            mode = Mode::Interactive;
+        } else if (modeStr == "from_file") {
+            mode = Mode::FromFile;
+            qtr::emptyArgument(inputFile, "Please specify input_file option");
+        } else {
+            LOG(ERROR) << "Bad mode option value";
+            exit(-1);
+        }
+
+        ansCount = absl::GetFlag(FLAGS_ans_count);
+
         for (auto &dir: dataDirPaths) {
             dbDataDirsPaths.emplace_back(dir / dbName);
         }
@@ -88,6 +123,84 @@ struct Args {
     }
 };
 
+void runInteractive(const qtr::BallTree &ballTree, const std::vector<std::string> &smilesTable,
+                    qtr::TimeTicker &timeTicker, const Args &args) {
+    while (true) {
+        std::cout << "Enter smiles: ";
+        std::string smiles;
+        std::cin >> smiles;
+        if (smiles.empty())
+            break;
+        timeTicker.tick();
+        qtr::IndigoFingerprint fingerprint;
+        try {
+            fingerprint = qtr::IndigoFingerprintFromSmiles(smiles);
+        }
+        catch (std::exception &exception) {
+            std::cout << "skip query:" << exception.what() << std::endl;
+            continue;
+        }
+
+        auto ans = ballTree.search(fingerprint, args.ansCount, args.startSearchDepth);
+        std::cout << "found answers: " << ans.size() << std::endl;
+        size_t answersToPrint = std::min(size_t(10), ans.size());
+        for (size_t i = 0; i < answersToPrint; i++) {
+            if (i == 0)
+                std::cout << "[";
+            std::cout << '\"' << smilesTable[ans[i]] << '\"';
+            std::cout << (i + 1 == answersToPrint ? "]\n" : ", ");
+        }
+        timeTicker.tick("Search time");
+    }
+}
+
+void runFromFile(const qtr::BallTree &ballTree, const std::vector<std::string> &smilesTable,
+                 qtr::TimeTicker &timeTicker, const Args &args) {
+    std::ifstream input(args.inputFile);
+    std::vector<std::string> queries;
+    while (input.peek() != EOF) {
+        std::string query;
+        input >> query;
+        std::string otherInfoInLine;
+        std::getline(input, otherInfoInLine);
+        queries.emplace_back(query);
+    }
+    std::vector<double> times;
+    size_t skipped;
+    LOG(INFO) << "Loaded " << queries.size() << " queries";
+    for (size_t i = 0; i < queries.size(); i++) {
+        LOG(INFO) << "Start search for " << i << ": " << queries[i];
+        timeTicker.tick();
+        qtr::IndigoFingerprint fingerprint;
+        try {
+            fingerprint = qtr::IndigoFingerprintFromSmiles(queries[i]);
+        }
+        catch (std::exception &exception) {
+            std::cout << "skip query:" << exception.what() << std::endl;
+            skipped++;
+            continue;
+        }
+        auto ans = ballTree.search(fingerprint, args.ansCount, args.startSearchDepth);
+        times.emplace_back(timeTicker.tick("search molecule " + std::to_string(i) + ": " + queries[i]));
+    }
+
+    double mean = std::accumulate(times.begin(), times.end(), 0.0) / times.size();
+    double min = *std::min_element(times.begin(), times.end());
+    double max = *std::max_element(times.begin(), times.end());
+    std::sort(times.begin(), times.end());
+    double median = times.size() & 1 ? times[times.size() / 2] : times[times.size() / 2] + times[times.size() / 2 + 1];
+
+    LOG(INFO) << "  mean: " << mean;
+    LOG(INFO) << "   max: " << max;
+    LOG(INFO) << "   min: " << min;
+    LOG(INFO) << "median: " << median;
+
+    std::cout << "  mean: " << mean << std::endl;
+    std::cout << "   max: " << max << std::endl;
+    std::cout << "   min: " << min << std::endl;
+    std::cout << "median: " << median << std::endl;
+}
+
 int main(int argc, char *argv[]) {
     initLogging(argc, argv);
     Args args(argc, argv);
@@ -103,38 +216,10 @@ int main(int argc, char *argv[]) {
     qtr::BallTree ballTree(ballTreeReader, args.dbDataDirsPaths);
     timeTicker.tick("DB initialization");
 
-    while (true) {
-        std::cout << "Enter smiles: ";
-        std::string smiles;
-        std::cin >> smiles;
-        if (smiles.empty())
-            break;
-        std::cout << "Enter andCount (0 - find all): ";
-        uint64_t ansCount;
-        std::cin >> ansCount;
-        if (ansCount == 0)
-            ansCount = -1;
-        timeTicker.tick();
-        qtr::IndigoFingerprint fingerprint;
-        try {
-            fingerprint = qtr::IndigoFingerprintFromSmiles(smiles);
-        }
-        catch (std::exception &exception) {
-            std::cout << "skip query:" << exception.what() << std::endl;
-            continue;
-        }
-
-        auto ans = ballTree.search(fingerprint, ansCount, args.startSearchDepth);
-        std::cout << "found answers: " << ans.size() << std::endl;
-        size_t answersToPrint = std::min(size_t(10), ans.size());
-        for (size_t i = 0; i < answersToPrint; i++) {
-            if (i == 0)
-                std::cout << "[";
-            std::cout << '\"' << smilesTable[ans[i]] << '\"';
-            std::cout << (i + 1 == answersToPrint ? "]\n" : ", ");
-        }
-        timeTicker.tick("Search time");
-    }
+    if (args.mode == Args::Mode::Interactive)
+        runInteractive(ballTree, smilesTable, timeTicker, args);
+    else if (args.mode == Args::Mode::FromFile)
+        runFromFile(ballTree, smilesTable, timeTicker, args);
 
     return 0;
 }
