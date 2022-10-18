df58d66451344174c2c5d807f0d138815f2fd423
dmitriy.yakovlev <dmitriy.yakovlev@quantori.com>
2022-10-18 15:48:24 +0300

    added port

--- cpp/app/main.cpp
+++ cpp/app/main.cpp
@@ -11,12 +11,16 @@ using namespace qtr;
 ABSL_FLAG(std::string, database_path, "",
           "Path to molecular database");
 
+ABSL_FLAG(size_t, port, 8080,
+          "Web service port");
+
 int main(int argc, char *argv[]) {
     
     google::InitGoogleLogging(argv[0]);
     absl::ParseCommandLine(argc, argv);
 
     std::string databasePath = absl::GetFlag(FLAGS_database_path);
+    auto servicePort = static_cast<size_t>(absl::GetFlag(FLAGS_port));
 
     emptyArgument(databasePath, "Please specify database_path option");
 
@@ -26,7 +30,7 @@ int main(int argc, char *argv[]) {
         return databasePath;
     });
 
-    app.port(18080).multithreaded().run();
+    app.port(servicePort).multithreaded().run();
 
     return 0;
 }
