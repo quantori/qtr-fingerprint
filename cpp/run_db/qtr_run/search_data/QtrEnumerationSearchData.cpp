72f6bd1620c792f3fbc293f6b92982a6e449bddb
Vsevolod Vaskin <vaskin.2003@gmail.com>
2024-03-18 00:02:36 +0200

    Fix QtrEnumeration bug

--- cpp/run_db/qtr_run/search_data/QtrEnumerationSearchData.cpp
+++ cpp/run_db/qtr_run/search_data/QtrEnumerationSearchData.cpp
@@ -11,28 +11,29 @@ namespace qtr {
         auto queryData = make_unique<QueryDataWithFingerprint>(ansCount, timeLimit, fingerprint,
                                                                make_unique<AlwaysTrueFilter<CIDType>>());
 
-        auto searchFunction = [&queryData, this]() {
-            auto& queryFingerprint = queryData->getQueryFingerprint();
+        auto searchFunction = [this](QueryDataWithFingerprint& queryData) {
+            auto& queryFingerprint = queryData.getQueryFingerprint();
+            assert(queryFingerprint.size() == 1024);
             std::vector<CIDType> answers;
             for (size_t i = 0; i < _allFingerprints->size() && i < ansCount; i++) {
                 auto& fingerprint = _allFingerprints->at(i);
                 if (queryFingerprint <= fingerprint)
                     answers.emplace_back(i);
                 if (i & ((1 << 16) - 1)) { // magic constant
-                    queryData->addAnswers(answers);
+                    queryData.addAnswers(answers);
                     answers.clear();
-                    if (queryData->checkTimeOut())
+                    if (queryData.checkTimeOut())
                         break;
                 }
             }
-            queryData->addAnswers(answers);
-            if (queryData->checkTimeOut()) {
+            queryData.addAnswers(answers);
+            if (queryData.checkTimeOut()) {
                 LOG(INFO) << "Search stopped due to timeout";
             }
-            queryData->tagFinishTask();
+            queryData.tagFinishTask();
         };
 
-        auto task = async(std::launch::async, searchFunction);
+        auto task = async(std::launch::async, searchFunction, std::ref(*queryData));
         queryData->addTask(std::move(task));
         return std::move(queryData);
     }
