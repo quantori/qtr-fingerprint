791b698c80dfb19ee491378073f0fec19a98ae56
Vsevolod Vaskin <vaskin.2003@gmail.com>
2024-11-19 10:39:15 +0200

    Add isSubstucture profiling (not working)

--- cpp/experiment/search_engines/IndigoUtils.h
+++ cpp/experiment/search_engines/IndigoUtils.h
@@ -2,9 +2,11 @@
 
 #include "GlobalIndigoSession.h"
 #include "IndigoSubstructureMatcher.h"
+#include "Profiling.h"
 
 inline bool isSubstructure(const indigo_cpp::IndigoQueryMolecule &queryMolecule,
                     const indigo_cpp::IndigoMolecule &candidateMol) {
+    ProfileScope("isSubstructure");
     auto matcher = globalIndigoSession->substructureMatcher(candidateMol);
     return bool(indigoMatch(matcher.id(), queryMolecule.id()));
 }
